e05f607f1b43be845711fb026d4439c5dcd69387
cosmic20 <bhargavhadya@gmail.com>
2017-04-16 01:55:28 -0700

    Create Schedule for 1 person

--- app/src/main/java/com/example/tinohacks/connectfinal/CreateSchedule.java
+++ app/src/main/java/com/example/tinohacks/connectfinal/CreateSchedule.java
@@ -1,15 +1,30 @@
 package com.example.tinohacks.connectfinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateSchedule extends AppCompatActivity {
+
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_schedule);
 
+
+        save = (Button) findViewById(R.id.saveSchedule);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Home.class);
+                startActivity(i);
+            }
+        });
 
 
 
